4b72a9122a08a7d95a13b287f83ed634be31ec15
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-12-26 13:47:14 -0800

    correction 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N
-(upper N) from the database hbtn_0e_0_usa
 """
-from sys import argv
+python script that lists all states from the database hbtn_0e_0_usa with
+a given name
+"""
+
 import MySQLdb
+from sys import argv
 
-
-if __name__ == '__main__':
-    user, password, database = argv[1], argv[2], argv[3]
-    db = MySQLdb.connect(host="localhost", user=user,
-                         passwd=password, db=database)
-    db = db.cursor()
-    db.execute("""SELECT * FROM states
-    WHERE REGEXP_LIKE(name, '^N', 'c') ORDER BY id""")
-    r = db.fetchall()
-    for i in r:
-        print(i)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    db.close()
 
