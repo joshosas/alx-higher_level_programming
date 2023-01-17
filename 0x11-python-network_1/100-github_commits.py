6a3af4e7f046b30843593d402f2e92db404b7ab5
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2023-01-17 14:05:22 -0800

    mandatory and advanced tasks

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) 
+of the repository “rails” by the user “rails”
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    l = r.json()
+    try:
+        for i in range(10):
+            print(l[i]
+    except:
+        pass
