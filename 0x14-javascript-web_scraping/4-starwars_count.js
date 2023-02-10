37a25b647e6349f232af312f2fdaecc7d740f832
Aigbiremhon Joshua <joshuaosaro20@gmail.com>
2023-02-10 02:35:23 +0000

    correction

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
+const args = require('process').argv;
 const request = require('request');
-const url = process.argv[2];
-request(url, (err, res, body) => {
+
+request(args[2], (err, res, body) => {
   if (err) {
     console.log(err);
     return;
   }
-  const films = JSON.parse(body).results;
+
+  body = JSON.parse(body);
   const wedgeAntilles = 'https://swapi-api.alx-tools.com/api/people/18/';
   let count = 0;
-  for (const movie of films) {
-    if (movie.characters.includes(wedgeAntilles)) {
-      count++;
-    }
+  for (const movie of body.results) {
+    if (movie.characters.includes(wedgeAntilles)) count++;
   }
   console.log(count);
 });
