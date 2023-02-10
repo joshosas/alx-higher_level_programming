c617db88a5c0e78a709016f11f0a7750b1f2cd95
Aigbiremhon Joshua <joshuaosaro20@gmail.com>
2023-02-10 00:54:20 +0000

    correction

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-let episode = process.argv[2];
-let url = `https://swapi-api.alx-tools.com/api/films/:${episode}`;
-request(url + episode, function (err, res, body) {
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (err, res, body) => {
   if (err) {
-    console.log(err);
-  } else if (res.statusCode === 200) {
-    let jsonObj = JSON.parse(body);
-    console.log(jsonObj.title);
+    console.error(err);
   } else {
-    console.log('An error occured. Status code: ' + res.statusCode);
+    console.log(JSON.parse(body).title);
   }
 });
